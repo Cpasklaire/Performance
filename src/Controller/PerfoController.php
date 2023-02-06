bc07b594e749f1f8a4b8f61afae10eca2c1d9753
Cpasklaire <80284454+Cpasklaire@users.noreply.github.com>
2023-02-06 15:53:21 +0100

    Initial commit

---
+++ src/Controller/PerfoController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PerfoController extends AbstractController
+{
+    #[Route('/', name: 'home')]
+    public function index(): Response
+    {
+        return $this->render('perfo/index.html.twig');
+    }
+
+    #[Route('/perfo', name: 'long_script')]
+    public function script(): Response
+    {
+        $start = microtime(true);
+
+        sleep(100);
+
+        /*Ce que je voudrais
+        $sleep1 = sleep(50);
+        $sleep2 = sleep(50);
+        $letout = await all([$sleep2, $sleep1]) */
+        /* idées : 
+        Symfony Messenger : https://symfony.com/doc/current/the-fast-track/fr/18-async.html 
+        serveur RabbitMQ : https://medium.com/@404sd/how-to-send-notification-asynchronously-using-symfony-rabbitmq-43e3077e9b36
+         */
+
+        $end = microtime(true);
+        $temps = $end - $start;
+        $response = new Response();
+        $response->setContent('Le script a pris ' . $temps . ' secondes.');
+
+        return $this->render('perfo/index.html.twig', [
+            'temps' => $response,
+        ]);
+    }
+}
